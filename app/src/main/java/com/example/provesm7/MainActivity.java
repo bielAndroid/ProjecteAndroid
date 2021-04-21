fe09051f6b9ba3dcdbf1860e307fe9bfd01a5cf4
Biel Comas Saavedra <bielcomas1@gmail.com>
2021-04-21 12:00:42 +0200

    Vista inicial començada

--- app/src/main/java/com/example/provesm7/MainActivity.java
+++ app/src/main/java/com/example/provesm7/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.provesm7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
